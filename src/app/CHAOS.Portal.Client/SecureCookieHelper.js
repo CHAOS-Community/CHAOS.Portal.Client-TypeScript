4fa33529556bd0b45c520a57cf3fb7d42db40fb3
Jacob Poul Richardt <jacob@geckon.com>
2013-01-21 02:22:41 +0100

    Added SecureCookieHelper.

--- src/app/CHAOS.Portal.Client/SecureCookieHelper.js
+++ src/app/CHAOS.Portal.Client/SecureCookieHelper.js
@@ -1,19 +1,82 @@
-var Shapes;
-(function (Shapes) {
-
-    var Point = Shapes.Point = (function () {
-        function Point(x, y) {
-            this.x = x;
-            this.y = y;
-        }
-        Point.prototype.getDist = function () {
-            return Math.sqrt((this.x * this.x) + (this.y * this.y));
-        };
-        Point.origin = new Point(0, 0);
-        return Point;
-    })();
-
-})(Shapes || (Shapes = {}));
-
-var p = new Shapes.Point(3, 4);
-var dist = p.getDist(); 
+var CHAOS;
+(function (CHAOS) {
+    (function (Portal) {
+        (function (Client) {
+            var SecureCookieHelper = (function () {
+                function SecureCookieHelper() { }
+                SecureCookieHelper.COOKIE_LIFE_TIME_DAYS = 90;
+                SecureCookieHelper.DoesCookieExist = function DoesCookieExist() {
+                    return this.GetCookie() != null;
+                }
+                SecureCookieHelper.Login = function Login(callback, serviceCaller) {
+                    if (typeof callback === "undefined") { callback = null; }
+                    if (typeof serviceCaller === "undefined") { serviceCaller = null; }
+                    var _this = this;
+                    var login = this.GetCookie();
+                    if(login == null) {
+                        if(callback != null) {
+                            callback(false);
+                        }
+                        return;
+                    }
+                    var test = login.wer;
+                    Client.SecureCookie.Login(function (response) {
+                        if(response.Error == null) {
+                            _this.SetCookie(response.Result.Results[0].GUID, response.Result.Results[0].PasswordGUID, _this.COOKIE_LIFE_TIME_DAYS);
+                            if(callback != null) {
+                                callback(true);
+                            }
+                        } else {
+                            if(callback != null) {
+                                callback(false);
+                            }
+                        }
+                    }, login.GUID, login.PasswordGUID, serviceCaller);
+                }
+                SecureCookieHelper.Create = function Create(serviceCaller) {
+                    if (typeof serviceCaller === "undefined") { serviceCaller = null; }
+                    var _this = this;
+                    Client.SecureCookie.Create(function (response) {
+                        if(response.Error != null) {
+                            _this.SetCookie(response.Result.Results[0].GUID, response.Result.Results[0].PasswordGUID, _this.COOKIE_LIFE_TIME_DAYS);
+                        }
+                    }, serviceCaller);
+                }
+                SecureCookieHelper.GetCookie = function GetCookie() {
+                    var cookie = document.cookie;
+                    if(cookie == undefined || cookie == null) {
+                        return null;
+                    }
+                    var guidRegEx = /SecureCookieGUID\=(.+?)(?:;|$)/;
+                    var passwordRegex = /SecureCookieGUIDPassword\=(.+?)(?:;|$)/;
+                    var result = {
+                        GUID: "",
+                        PasswordGUID: ""
+                    };
+                    var match = guidRegEx.exec(cookie);
+                    if(match == null) {
+                        return null;
+                    }
+                    result.GUID = match[1];
+                    match = passwordRegex.exec(cookie);
+                    if(match == null) {
+                        return null;
+                    }
+                    result.PasswordGUID = match[1];
+                    return result;
+                }
+                SecureCookieHelper.SetCookie = function SetCookie(guid, passwordGUID, expireInDays) {
+                    var expireDate = new Date();
+                    expireDate.setDate(expireDate.getDate() + expireInDays);
+                    document.cookie = "SecureCookieGUID=" + (guid == null ? "" : guid) + "; expires=" + expireDate.toUTCString() + ";";
+                    document.cookie = "SecureCookieGUIDPassword=" + (passwordGUID == null ? "" : passwordGUID) + "; expires=" + expireDate.toUTCString() + ";";
+                }
+                return SecureCookieHelper;
+            })();
+            Client.SecureCookieHelper = SecureCookieHelper;            
+        })(Portal.Client || (Portal.Client = {}));
+        var Client = Portal.Client;
+    })(CHAOS.Portal || (CHAOS.Portal = {}));
+    var Portal = CHAOS.Portal;
+})(CHAOS || (CHAOS = {}));
+//@ sourceMappingURL=SecureCookieHelper.js.map
--- src/app/CHAOS.Portal.Client/SecureCookieHelper.ts
+++ src/app/CHAOS.Portal.Client/SecureCookieHelper.ts
@@ -3,8 +3,80 @@
 
 module CHAOS.Portal.Client
 {
-	export class MetadataSchema
+	export class SecureCookieHelper
 	{
+		private static COOKIE_LIFE_TIME_DAYS:number = 90;
 
+		public static DoesCookieExist(): bool
+		{
+			return this.GetCookie() != null;
+		}
+
+		public static Login(callback:(success:bool) => void = null, serviceCaller:CHAOS.Portal.Client.IServiceCaller = null):void
+		{ 
+			var login = this.GetCookie();
+
+			if (login == null)
+			{
+				if(callback != null) callback(false);
+				return;
+			}
+
+			SecureCookie.Login(response => {
+				if (response.Error == null)
+				{
+					this.SetCookie(response.Result.Results[0].GUID, response.Result.Results[0].PasswordGUID, this.COOKIE_LIFE_TIME_DAYS);
+					if(callback != null) callback(true);
+				}
+				else
+					if(callback != null) callback(false);
+			}, login.GUID, login.PasswordGUID, serviceCaller);
+		}
+
+		public static Create(serviceCaller:CHAOS.Portal.Client.IServiceCaller = null):void
+		{
+			SecureCookie.Create(response =>
+			{
+				if(response.Error != null)
+					this.SetCookie(response.Result.Results[0].GUID, response.Result.Results[0].PasswordGUID, this.COOKIE_LIFE_TIME_DAYS);
+			}, serviceCaller);
+		}
+
+		private static GetCookie(): { GUID: string; PasswordGUID: string; }
+		{
+			var cookie = document.cookie;
+
+			if (cookie == undefined || cookie == null)
+				return null;
+
+			var guidRegEx = /SecureCookieGUID\=(.+?)(?:;|$)/;
+			var passwordRegex = /SecureCookieGUIDPassword\=(.+?)(?:;|$)/;
+
+			var result = { GUID: "", PasswordGUID: "" };
+			var match = guidRegEx.exec(cookie);
+
+			if (match == null)
+				return null;
+
+			result.GUID = match[1];
+
+			match = passwordRegex.exec(cookie);
+
+			if (match == null)
+				return null;
+
+			result.PasswordGUID = match[1];
+
+			return result;
+		}
+
+		private static SetCookie(guid, passwordGUID, expireInDays):void
+		{
+			var expireDate = new Date();
+			expireDate.setDate(expireDate.getDate() + expireInDays);
+
+			document.cookie = "SecureCookieGUID=" + (guid == null ? "" : guid) + "; expires=" + expireDate.toUTCString() + ";";
+			document.cookie = "SecureCookieGUIDPassword=" + (passwordGUID == null ? "" : passwordGUID) + "; expires=" + expireDate.toUTCString() + ";";
+		}
 	}
 }
