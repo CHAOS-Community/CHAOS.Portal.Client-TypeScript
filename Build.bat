73d8bc33685378557e4e84421a9b7f0b3e696e8f
Jacob Poul Richardt <jacob@geckon.com>
2013-01-21 01:39:56 +0100

    Added build file.

---
+++ Build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Needed to update variable in loop
+setlocal enabledelayedexpansion
+
+rem Set current dir to bat file location
+CD /D %~dp0
+
+for %%i in (src\app\CHAOS.Portal.Client\*.js) do (set files=!files!%%~i )
+
+echo Merging and minifing CHAOS TypeScript SDK - %files% -
+
+tools\AjaxMin\AjaxMin.exe -CLOBBER:TRUE %files% -out build\PortalClient.min.js
+
+echo Done
---
+++ src/app/CHAOS.Portal.Client/SecureCookieHelper.ts
@@ -0,0 +1,10 @@
+/// <reference path="PortalClient.ts"/>
+/// <reference path="PortalExtensions.ts"/>
+
+module CHAOS.Portal.Client
+{
+	export class MetadataSchema
+	{
+
+	}
+}
